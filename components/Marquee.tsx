64ebf767c2f4835bcc7ab175e6169f640fc9354b
Michael Boland <michael@boland.onl>
2023-06-22 12:31:45 -0500

    Fix styles

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -6,8 +6,9 @@ export default function Marquee() {
    return (
       <section className='flex flex-col items-stretch my-12'>
          <div className="px-2 text-lg mb-2">Contributions</div>
-         {/* https://coolors.co/ 7 shades darker */}
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FD49A0] via-[#FD49A0] to-[#B60259] border-b hover:opacity-95">
+
+         {/* https://coolors.co/ 3 shades darker */}
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FD49A0] via-[#FD49A0] to-[#FD0D81] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
@@ -21,9 +22,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center uppercase">(NSFW)</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite lowercase font-normal">AI</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;23</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;23</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#DF2266] via-[#DF2266] to-[#6B0F31] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#DF2266] via-[#DF2266] to-[#B31952] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
@@ -35,9 +37,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">getmainline.io</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite lowercase font-normal">web3</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;23</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;23</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#369999] via-[#369999] to-[#153C3C] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#369999] via-[#369999] to-[#2A7979] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
@@ -50,9 +53,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">tronic.app</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite lowercase font-normal">web3</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;23</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;23</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#8BA242] via-[#8BA242] to-[#3E481E] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#8BA242] via-[#8BA242] to-[#708235] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyBlack flex">
                <Link
                   target="_blank"
@@ -65,9 +69,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">smartlockerusa.com</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyBlack border-easyBlack lowercase font-normal">agtech</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;23</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;23</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FA9556] via-[#FA9556] to-[#C65006] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FA9556] via-[#FA9556] to-[#F87625] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyBlack flex">
                <Link
                   target="_blank"
@@ -80,9 +85,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">ae.studio</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyBlack border-easyBlack lowercase font-normal">web3</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;22</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;22</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#1966FF] via-[#1966FF] to-[#00308F] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#1966FF] via-[#1966FF] to-[#004BE0] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
@@ -95,10 +101,11 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">the.com</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite border-easyWhite lowercase font-normal">no-code</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;22</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;22</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FFC728] via-[#FFC728] to-[#A37800] border-b hover:opacity-95">
-            <div className="text-sm leading-6 text-easyBlack flex">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#8D1D40] via-[#8D1D40] to-[#66152E] border-b hover:opacity-95">
+            <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
                   href="https://asu.edu"
@@ -111,11 +118,12 @@ export default function Marquee() {
                   <span aria-hidden="true" className="ml-px"><TbArrowUpRight /></span>
                </Link>
                <span className="ml-2 text-sm flex items-center lowercase">asu.edu</span>
-               <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyBlack border-easyBlack lowercase font-normal">e-learning</Badge>
+               <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite border-easyWhite lowercase font-normal">e-learning</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;22</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;22</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FFF202] via-[#FFF202] to-[#7A7400] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FFF202] via-[#FFF202] to-[#CCC200] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyBlack flex">
                <Link
                   target="_blank"
@@ -128,9 +136,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">blockearner.com.au</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyBlack border-easyBlack lowercase font-normal">web3</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;21</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;21</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#E21C11] via-[#E21C11] to-[#5F0C07] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#E21C11] via-[#E21C11] to-[#AB150D] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
@@ -143,9 +152,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">hy-vee.com</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite lowercase font-normal">e-commerce</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;21</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;21</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FAB303] via-[#FAB303] to-[#795701] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FAB303] via-[#FAB303] to-[#CA9102] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyBlack flex">
                <Link
                   target="_blank"
@@ -158,9 +168,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">gofastpath.com</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyBlack border-easyBlack lowercase font-normal">saas</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;19</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;19</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#0073D3] via-[#0073D3] to-[#002D52] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#0073D3] via-[#0073D3] to-[#005AA3] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
@@ -173,9 +184,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">corteva.com</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite border-easyWhite lowercase font-normal">agtech</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;19</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;19</div>
          </div>
-         <div className="flex items-start justify-between  px-6 py-3 bg-gradient-to-r from-[#17ABDA] via-[#17ABDA] to-[#0A495C] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between  px-6 py-3 bg-gradient-to-r from-[#17ABDA] via-[#17ABDA] to-[#1183A6] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
@@ -188,9 +200,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">capgemini.com</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite border-easyWhite lowercase font-normal">SAAS</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;19</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;19</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FE0000] via-[#FE0000] to-[#7A0000] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#FE0000] via-[#FE0000] to-[#CC0000] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
@@ -203,9 +216,10 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">swissep.org</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite border-easyWhite lowercase font-normal">ngo</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;17</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;17</div>
          </div>
-         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#0555FB] via-[#0555FB] to-[#022979] border-b hover:opacity-95">
+
+         <div className="flex items-start justify-between px-6 py-3 bg-gradient-to-r from-[#0555FB] via-[#0555FB] to-[#0345C9] border-b hover:opacity-95">
             <div className="text-sm leading-6 text-easyWhite flex">
                <Link
                   target="_blank"
@@ -221,8 +235,9 @@ export default function Marquee() {
                <span className="ml-2 text-sm flex items-center lowercase">thenewdigitalschool.com</span>
                <Badge variant="outline" className="ml-4 text-sm flex items-center text-easyWhite border-easyWhite lowercase font-normal">e-learning</Badge>
             </div>
-            <div className="justify-end text-easyWhite/80 hidden md:flex">&apos;17</div>
+            <div className="justify-end text-easyWhite/50 hidden md:flex">&apos;17</div>
          </div>
+
       </section>
    )
 }
