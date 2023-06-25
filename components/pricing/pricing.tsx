79e91225d406751aae5207b31af5150174219f05
Michael Boland <michael@boland.onl>
2023-06-25 15:53:38 -0500

    Fix spacing

--- components/pricing/pricing.tsx
+++ components/pricing/pricing.tsx
@@ -4,9 +4,9 @@ const pricingMethods = [
    {
       key: 'hourly',
       name: 'Hourly',
-      price: '$205/h',
+      price: '$205/hr',
       description: (
-         <div className="space-y-1 my-4">
+         <div className="space-y-2 my-4">
             <p>Pay for the time spent on your project.</p>
             <p>The most common pricing method for software development services.</p>
             <p>Suitable for projects with a high level of uncertainty or requiring scope changes during the development process.</p>
@@ -26,7 +26,7 @@ const pricingMethods = [
       name: 'Project-based',
       price: '$7,995+',
       description: (
-         <div className="space-y-1 my-4">
+         <div className="space-y-2 my-4">
             <p>Pay a fixed price for the whole project.</p>
             <p>Suitable for projects with a low level of uncertainty and defined deadlines.</p>
          </div>
@@ -45,7 +45,7 @@ const pricingMethods = [
       name: 'Value-based',
       price: '$4,995+',
       description: (
-         <div className="space-y-1 my-4">
+         <div className="space-y-2 my-4">
             <p>Pay a lower fixed price for the whole project as well as a mutually-agreed fee per transaction, sale, or other metric.</p>
             <p>The total price is more affordable as the risk, and therefore reward, is shared between parties.</p>
             <p>Suitable pricing method for projects with a high level of uncertainty and a high potential for growth.</p>
@@ -63,9 +63,9 @@ const pricingMethods = [
    {
       key: 'subscription-based',
       name: 'Subscription-based (DAAS)',
-      price: '$6,995/m',
+      price: '$6,995/mo',
       description: (
-         <div className="space-y-1 my-4">
+         <div className="space-y-2 my-4">
             <p>Pay a monthly fee for dedicated development services.</p>
             <p>The monthly price is fixed, predictable, and more affordable than hiring a full-time developer.</p>
             <p>Suitable pricing method for those with light to moderate needs and unpredictable work inflows.</p>
