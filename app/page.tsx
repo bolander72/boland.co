76387cc20091e21bc5d849c5697e8e528869e26d
Michael Boland <michael@boland.onl>
2023-06-26 23:39:17 -0500

    Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,34 @@ import Pricing from '@/components/pricing/pricing'
 export const metadata = {
   title: 'Boland Co.',
   description: 'an indie web studio.',
+  keywords: ['Next.js', 'Web studio', 'Web Agency', 'Vercel', 'Tailwind CSS', 'shadcn-ui', 'DAAS', 'Development-as-a-service', 'bitcoin', 'lightning', 'web3', 'ai'],
+  authors: [{ name: 'Michael Boland', url: 'https://boland.co' }],
+  creator: 'Michael Boland',
+  openGraph: {
+    images: '/og.jpg',
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: 'Boland Co.',
+      description: 'an indie web studio.',
+      siteId: '1656054845290295298',
+      creator: '@bolandonl',
+      creatorId: '1656054845290295298',
+      images: ['https://boland.co/og.jpg'],
+    },
+  },
 }
 
 export default function Home() {
