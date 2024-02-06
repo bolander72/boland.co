9ed3bd130384b38c568612c7bf2f45767ba0da47
Michael Boland <michael@boland.co>
2024-02-05 21:07:29 -0600

    Setup blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,60 +12,7 @@ export default function Blog() {
   return (
     <main>
       <section className='space-y-6'>
-        <Title className='text-3xl'>Hello, I'm Michael.</Title>
-        <Paragraph>
-          I'm a frontend developer, product strategist, and independent
-          consultant. I also write a{' '}
-          <Link href='/blog' className='text-blue-600 underline'>
-            blog
-          </Link>
-          , which has existed in scribbles, physical notes, and journals for
-          years (and digitally since 2023).
-        </Paragraph>
-        <Paragraph>
-          As a developer, I build applications across web and mobile, do
-          contract work for various clients, and build startups and side
-          projects. To read more, please head to my{' '}
-          <Link href='/work' className='text-blue-600 underline'>
-            work
-          </Link>{' '}
-          page covering more of the details.
-        </Paragraph>
-        <Paragraph>
-          As a product strategist, I work with founders and companies to turn
-          ideas into products. This includes things like research and analysis,
-          product discovery and design, defining engingeering processes, brand
-          strategy, and more.
-        </Paragraph>
-        <Paragraph>
-          In my consulting practice, I partner with companies to empower
-          leaders, cultivate more efficient and effective practices, and
-          increase autonomy and freedom for teams. A more holistic explanation
-          can be found{' '}
-          <Link href='/work' className='text-blue-600 underline'>
-            here
-          </Link>
-          .
-        </Paragraph>
-      </section>
-      <section className='mt-6 space-y-6'>
-        <div>
-          <Paragraph>Want to chat?</Paragraph>
-          <Paragraph>
-            Book a time{' '}
-            <Link
-              href='https://cal.com/boland/chat'
-              className='text-blue-600 underline'
-            >
-              here
-            </Link>
-            .
-          </Paragraph>
-        </div>
-        <div>
-          <Paragraph>Send an email:</Paragraph>
-          <Paragraph>hello [at] boland [dot] co.</Paragraph>
-        </div>
+        <Title className='text-3xl'>Blog</Title>
       </section>
     </main>
   )
