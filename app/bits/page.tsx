620c9056637cc24bb95b57e696ff86ff7f77a18f
Michael Boland <michael@boland.co>
2024-06-04 09:21:13 -0500

    Add bits

--- app/bits/page.tsx
+++ app/bits/page.tsx
@@ -1,26 +1,31 @@
-'use client'
-
 import { Title } from '@/components/title'
-import { posts } from '@/.velite'
 import Link from 'next/link'
 import Post from '@/components/post'
-import sharedMetadata from '@/metadata'
-import useNoisyCover from '@/hooks/use-noisy-cover'
+
+const bits = [
+   {
+      title: 'Noisy Cover',
+      description: 'Generate a noisy visual',
+      link: '/bits/noisy-cover',
+   },
+   {
+      title: 'Noisy Shapes',
+      description: 'Generate a noisy shapes visual',
+      link: '/bits/noisy-shapes',
+   },
+]
 
 export default function Page() {
-   const { id } = useNoisyCover()
-
    return (
-      <section className='w-full space-y-6'>
+      <section className='space-y-6'>
          <Title>Bits</Title>
-         <div className='relative'>
-            <Link href='/bits/noisy-cover'>
-               <canvas className='aspect-video w-full rounded-2xl border' id={id} />
-               <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-primary text-2xl font-semibold bg-secondary p-3 rounded-lg'>
-                  Noisy Cover
-               </div>
-            </Link>
-         </div>
+         {bits.map(bit => (
+            <div key={bit.title}>
+               <Link href={bit.link}>
+                  <Post post={bit} showDate={false} />
+               </Link>
+            </div>
+         ))}
       </section>
    )
 }
