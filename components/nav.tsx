ea83c723fb67e090ef644fef73c6b326846241af
Michael Boland <michael@boland.co>
2024-06-13 18:51:55 -0500

    Bump packages

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,10 +6,7 @@ export function Nav() {
     <nav className='mx-auto my-12 max-w-lg px-4'>
       <div className='flex w-full items-center justify-between'>
         <div className='space-x-4'>
-          <Link
-            href='/'
-            className='text-blue-600 underline dark:text-blue-500'
-          >
+          <Link href='/' className='text-blue-600 underline dark:text-blue-500'>
             about
           </Link>
           <Link
