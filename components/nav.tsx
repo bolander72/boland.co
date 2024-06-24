83326321f8a16035c0fe54b28f8015afa4c94892
Michael Boland <michael@boland.co>
2024-06-23 19:27:58 -0500

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,7 +6,7 @@ export function Nav() {
     <nav className='mx-auto my-12 max-w-[430px] px-4'>
       <div className='flex w-full items-center justify-between'>
         <div className='space-x-4 text-lg'>
-          <CustomLink href='/'>/</CustomLink>
+          <CustomLink href='/'>about</CustomLink>
           <CustomLink href='/blog'>blog</CustomLink>
         </div>
       </div>
