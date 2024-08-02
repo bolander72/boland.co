901568717b8f2e138ef9b04f03dcec01687d6988
Michael Boland <michael@boland.co>
2024-08-01 20:39:57 -0500

    Disable rss prefetch

--- components/blog-title.tsx
+++ components/blog-title.tsx
@@ -6,7 +6,7 @@ export default function BlogTitle() {
   return (
     <div className='flex items-baseline space-x-1'>
       <AnimatedTitle text='Blog' className='text-left' />
-      <CustomLink href='/rss'>
+      <CustomLink href='/rss' prefetch={false}>
         <Rss className='h-5 w-5 animate-fade animate-delay-[1500ms] animate-duration-[2000ms]' />
       </CustomLink>
     </div>
