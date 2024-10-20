f69abaf683293a34c0ff8682de6d017674466115
Michael Boland <michael@boland.co>
2024-10-20 13:20:28 -0500

    Cleanup home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,6 @@
 import { Title } from '@/components/title'
-import LinkSection from '@/components/link-section'
 import { MDXContent } from '@/components/mdx-content'
 import { pages } from '@/.velite'
-
-const currentWorkLinks = [
-  { name: 'Carry', href: 'https://carry.com' },
-  { name: 'Mainline', href: 'https://getmainline.io' },
-  { name: 'Bitcoin Layers', href: 'https://bitcoinlayers.org' }
-]
-
-const elsewhereLinks = [
-  { name: 'X', href: 'https://x.com/bolander72', displayText: '@bolander72' },
-  {
-    name: 'Nostr',
-    href: 'https://primal.net/p/npub1ge93aqdp0rjewv2tq7f7u5d5mv4p78w2d74539fsjvr042cwcntsqxgusd',
-    displayText: 'npub...usd'
-  },
-  {
-    name: 'GitHub',
-    href: 'https://github.com/bolander72',
-    displayText: '@bolander72'
-  },
-  {
-    name: 'LinkedIn',
-    href: 'https://linkedin.com/in/bolander72/',
-    displayText: '@bolander72'
-  }
-]
-
-const blogLinks = [
-  { name: 'Ribbonfarm', href: 'https://ribbonfarm.com' },
-  { name: 'Jihad Esmail', href: 'https://jihad.house' },
-  { name: 'Product Lost', href: 'https://hipcityreg.substack.com' },
-  { name: 'Elisabeth Nicula', href: 'https://elisabethnicula.com' }
-]
 
 export default function Page() {
   const page = pages.find(page => page.slug === 'home')
