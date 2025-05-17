ce9ea578051b43c542249f8aa1030572373c0cc2
Michael Boland <23284377+bolander72@users.noreply.github.com>
2025-05-17 00:04:31 -0500

    Use Astro

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [sitemap(), react()],
+    output: "static",
+    adapter: vercel(),
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': '/src'
+            }
+        }
+    },
+    viewTransitions: true,
+});
---
+++ src/components/testimonial-card.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import {
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+interface Props {
+	content: string[];
+	authorName: string;
+	authorTitle: string;
+	authorCompany: string;
+	authorImageSrc: string;
+}
+
+export default function TestimonialCard({
+	content,
+	authorName,
+	authorTitle,
+	authorCompany,
+	authorImageSrc,
+}: Props) {
+	return (
+		<Card className="shadow-none">
+			<CardContent className="space-y-4">
+				{content.map((paragraph, index) => (
+					<p key={index} className="!text-sm">
+						{paragraph}
+					</p>
+				))}
+			</CardContent>
+			<CardHeader className="flex flex-row items-center justify-between">
+				<div className="flex flex-col gap-1">
+					<CardTitle className="!font-normal">{authorName}</CardTitle>
+					<CardDescription className="!font-normal">
+						{authorTitle}, {authorCompany}
+					</CardDescription>
+				</div>
+				<CardAction>
+					<img
+						src={authorImageSrc}
+						alt={authorName}
+						className="h-15 w-15 rounded-full border"
+					/>
+				</CardAction>
+			</CardHeader>
+		</Card>
+	);
+}
