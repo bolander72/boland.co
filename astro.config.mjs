78862bd7f012a041d5ca0a1a8fcd4c99d6319ae9
Michael Boland <23284377+bolander72@users.noreply.github.com>
2025-06-13 20:19:30 -0500

    Update font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
@@ -19,4 +19,11 @@ export default defineConfig({
         }
     },
     viewTransitions: true,
+    experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Figtree",
+            cssVariable: "--font-figtree"
+        }]
+    }
 });
